18def151763e1416ec1f975506888c4a844d8fa4
chenhaofen <670581641@qq.com>
2017-08-11 15:30:41 +0800

    第二次修改

--- src/testgut/t/dasd.java
+++ src/testgut/t/dasd.java
@@ -3,5 +3,6 @@ package testgut.t;
 public class dasd {
 	public static void main(String[] args) throws InterruptedException {
 		Thread.sleep(1000);
+		System.out.println("222222222222222222222222222");
 	}
 }
